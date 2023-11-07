af788ff4876fd67abe4c44dc7c649af1ed803b0f
AlisherMansurov <alisher.mansurov101102@gmail.com>
2023-11-07 17:22:08 +0600

    new file

---
+++ Mod10prac/BuildLib/Walls.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildLib
+{
+    public class Walls : IPart
+    {
+        public bool Ready { get; set; } = false;
+        public void Build()
+        {
+            Console.WriteLine("Стена готова");
+            Ready = true;
+        }
+
+        public void Info()
+        {
+            if (Ready)
+            {
+                Console.WriteLine("Стена построена");
+            }
+            else
+            {
+                Console.WriteLine("Стена не построена");
+            }
+        }
+    }
+}
---
+++ Mod10prac/BuildLib/Window.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildLib
+{
+    public class Window : IPart
+    {
+        public bool Ready { get; set; }=false;
+        public void Build()
+        {
+            Console.WriteLine("Окно готово");
+            Ready = true;
+        }
+
+        public void Info()
+        {
+            if (Ready)
+            {
+                Console.WriteLine("Окно построено");
+            }
+            else
+            {
+                Console.WriteLine("Окно не построено");
+            }
+        }
+    }
+}
