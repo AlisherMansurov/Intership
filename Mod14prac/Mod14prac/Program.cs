290e76af3f2eae428c80c21ffecef8426aa26f49
AlisherMansurov <alisher.mansurov101102@gmail.com>
2023-12-02 20:01:06 +0600

    new file"

---
+++ Mod14prac/Mod14prac/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod14prac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Вот дом, Который построил Джек. А это пшеница, Которая в темном чулане хранится В доме, Который построил Джек. А это веселая птица синица, Которая часто ворует пшеницу, Которая в темном чулане хранится В доме, Который построил Джек.";
+
+            Dictionary<string, int> wordCount = CountWords(text);
+
+            DisplayStatistics(wordCount);
+        }
+
+        static Dictionary<string, int> CountWords(string text)
+        {
+            Dictionary<string, int> wordCount = new Dictionary<string, int>(StringComparer.OrdinalIgnoreCase);
+            string[] words = text.Split(new char[] { ' ', '.', ',', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string word in words)
+            {
+                if (wordCount.ContainsKey(word))
+                {
+                    wordCount[word]++;
+                }
+                else
+                {
+                    wordCount[word] = 1;
+                }
+            }
+
+            return wordCount;
+        }
+
+        static void DisplayStatistics(Dictionary<string, int> wordCount)
+        {
+            Console.WriteLine("| Слово          | Количество |");
+            Console.WriteLine("|----------------|------------|");
+
+            foreach (var kvp in wordCount.OrderByDescending(x => x.Value))
+            {
+                Console.WriteLine($"| {kvp.Key,-15} | {kvp.Value,-10} |");
+            }
+        }
+    }
+}
